6b9ca70753431be265acf8dfeb9f462ba11a0f7d
lmx <1035234303@qq.com>
2019-12-05 15:47:24 +0800

    add turtle_test.py

---
+++ day01/turtle_test.py
@@ -0,0 +1,17 @@
+'''
+turtle是Python内置的绘图logo语言。
+'''
+import turtle
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
+
